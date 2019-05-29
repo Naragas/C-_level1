7bda1eb49f33d271fc3d7c53897aea6c49c0c469
Naragas <naragas@gmail.com>
2019-05-30 01:23:46 +0300

    Начал выполнять ДЗ

---
+++ lesson_1/Lesson1_HW/Lesson1_HW/Library.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_HW
+{
+    class Library
+    {
+        public static void Print(String text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static void Pause()
+        {
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
---
+++ lesson_1/Lesson1_HW/Lesson1_HW/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson1_HW;
+
+namespace Lesson1_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library.Print("Введите ваше имя: ");
+            string fName = Console.ReadLine();
+            Library.Print("Введите вашу фамилию: ");
+            string lName = Console.ReadLine();
+            Library.Print("Введите ваш возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Library.Print("Введите ваш рост: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Library.Print("Введите ваш вес: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            Library.Print("Имя: " + fName + ", Фамилия: " + lName + ", Возраст: " + age + ", Рост: " + height + ", Вес: " + weight);
+            Library.Pause();
+
+        }
+    }
+}
