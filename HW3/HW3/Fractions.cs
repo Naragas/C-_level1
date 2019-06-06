5051352989ad41a82d3d3086b0f4b12ac0e683e1
Naragas <naragas@gmail.com>
2019-06-06 17:38:12 +0300

    Доработка 3ей задачи.

---
+++ HW3/HW3/Fractions.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    class Fractions
+    {
+        private int numenator;
+        private int denominator;
+
+        public int NumenatorGet()
+        {
+            return numenator;
+        }
+        public void NumenatorSet(int value)
+        {
+            numenator = value;
+        }
+
+        public int DemonatorGet()
+        {
+            return denominator;
+        }
+        public void DenominatorSet(int value)
+        {
+            denominator = value;
+        }
+
+        public Fractions()
+        {
+
+        }
+
+        public Fractions(int numenator, int denominator)
+        {
+            if(denominator == 0)
+            {
+                throw new ArgumentNullException("Fraction denominator can't be zero");
+            }
+
+            this.numenator = numenator;
+            this.denominator = denominator;
+        }
+
+        static int NOD(Fractions f2)
+        {
+            int a = f2.numenator;
+            int b = f2.denominator;
+
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+            return a;
+        }
+
+        public double decimalFraction()
+        {
+            return (double)numenator / (double)denominator;
+
+        }
+
+        public Fractions Simplification(Fractions f2)
+        {
+            Fractions f3 = new Fractions();
+            f3.numenator = f2.numenator / NOD(f2);
+            f3.denominator = f2.denominator / NOD(f2);
+
+            return f3;
+
+        }
+
+
+        public Fractions Summ(Fractions f2)
+        {
+            Fractions f3 = new Fractions();
+            f3.numenator = numenator * f2.denominator + f2.numenator * denominator;
+            f3.denominator = denominator * f2.denominator;
+
+            return f3.Simplification(f3);
+        }
+
+        public Fractions Diff(Fractions f2)
+        {
+            Fractions f3 = new Fractions();
+            f3.numenator = numenator * f2.denominator - f2.numenator * denominator;
+            f3.denominator = denominator * f2.denominator;
+
+            return f3.Simplification(f3);
+        }
+
+        public Fractions Mult(Fractions f2)
+        {
+            Fractions f3 = new Fractions();
+            f3.numenator = numenator * f2.numenator;
+            f3.denominator = denominator * f2.denominator;
+
+            return f3.Simplification(f3);
+        }
+
+        public Fractions Divi(Fractions f2)
+        {
+            Fractions f3 = new Fractions();
+            f3.numenator = numenator * f2.denominator;
+            f3.denominator = denominator * f2.numenator;
+
+            return f3.Simplification(f3);
+        }
+
+        public String Show()
+        {
+            return numenator + "/" + denominator;
+        }
+
+    }
+}
