deaadadfb69412943618a93b28a0456a2dce128f
Naragas <naragas@gmail.com>
2019-06-11 01:00:56 +0300

    Выполнено задание 5 к уроку номер 4.

--- HW4/HW4_5/Program.cs
+++ HW4/HW4_5/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyTwoDimensionalArrayLibrary;
 
 /*а) Реализовать библиотеку с классом для работы с двумерным массивом. 
  * Реализовать конструктор, заполняющий массив случайными числами. 
@@ -22,6 +23,34 @@ namespace HW4_5
     {
         static void Main(string[] args)
         {
+            
+            string PathToFile = @"C:\Users\{user}\Downloads\two-dim-3x5.csv";
+                      
+            string Directory = @"C:\Users\{user}\Downloads\";
+
+
+
+            MyTwoDimensionalArray TwoDimArray = new MyTwoDimensionalArray(PathToFile);
+            Console.WriteLine(TwoDimArray.ToString());
+
+            Console.WriteLine($"Максимальный элемент: {TwoDimArray.Max}");
+            Console.WriteLine($"Минимальный элемент: {TwoDimArray.Min}");
+
+            
+            Console.WriteLine($"Сумма всех значений: {TwoDimArray.Summ()}");
+
+            ;
+            Console.WriteLine($"Сумма всех значений, начиная с {5}: {TwoDimArray.SummMoreThan(5)}");
+
+            List<int> indexNumber = new List<int>();
+
+
+            TwoDimArray.MaxElementNumber(ref indexNumber);
+            Console.WriteLine($"Индекс максимального элемента: {indexNumber}");
+
+            Console.WriteLine($"Данные выгружены в: [{TwoDimArray.OutputdataToDirectory(Directory)}]");
+
+            Console.ReadKey();
         }
     }
 }
--- HW4/MyTwoDimensionalArrayLibrary/MyTwoDimensionalArray.cs
+++ HW4/MyTwoDimensionalArrayLibrary/MyTwoDimensionalArray.cs
@@ -6,106 +6,248 @@ namespace MyTwoDimensionalArrayLibrary
 {
     public class MyTwoDimensionalArray
     {
-        private int[,] a;
-        Random rnd = new Random();
+        /*       private int[,] Data { get; }
 
-        public MyTwoDimensionalArray(int n, int m)
-        {
-            a = new int[n, m];
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    a[i, j] = rnd.Next(1, 50);
+               Random rnd = new Random();
 
-                }
-            }
-        }
+               public MyTwoDimensionalArray(int n, int m)
+               {
+                   Data = new int[n, m];
+                   for (int i = 0; i < n; i++)
+                   {
+                       for (int j = 0; j < m; j++)
+                       {
+                           Data[i, j] = rnd.Next(1, 50);
 
-        public MyTwoDimensionalArray(string filePath)
-        {
-            StreamReader streamReader = new StreamReader(filePath);
-            while (!streamReader.EndOfStream)
-            {
+                       }
+                   }
+               }
 
-            }
+               public MyTwoDimensionalArray(String fileName)
+               {
+                   (uint rowsTotal, uint columnsTotal) = ParseDimentions(fileName);
+                   CheckDimentions(rowsTotal, columnsTotal);
 
-        }
+                   Data = new int[rowsTotal, columnsTotal];
+                   foreach (var (row,column,value) in ReadFileSafe(fileName))
+                   {
+                       Data[row, column] = value;
+                   }
 
-        public int Summ()
-        {
-            int result = 0;
-            for (int i = 0; i < a.GetLength(0); i++)
-            {
-                for (int j = 0; j < a.GetLength(1); j++)
-                {
-                    result += a[i, j];
-                }
-            }
-            return result;
-        }
 
-        public int SummMoreThan(int n)
-        {
-            int result = 0;
-            for (int i = 0; i < a.GetLength(0); i++)
-            {
-                for (int j = 0; j < a.GetLength(1); j++)
-                {
-                    if (a[i, j] > n)
-                    {
-                        result += a[i, j];
-                    }
-                }
-            }
-            return result;
-        }
+               }
 
-        public int Min
-        {
-            get
-            {
-                int min = a[0, 0];
-                for (int i = 0; i < a.GetLength(0); i++)
-                {
-                    for (int j = 0; j < a.GetLength(1); j++)
-                    {
-                        if (a[i, j] < min) min = a[i, j];
-                    }
-                }
-                return min;
-            }
+               private void CheckDimentions(uint numberOfRows, uint numberOfColumns)
+               {
+                   if (numberOfRows == 0 || numberOfColumns == 0)
+                   {
+                       throw new ArgumentException("Cant create zero length two-dim array");
+                   }
+               }
 
-        }
+               private static IEnumerable<(int row, int column, int value)> ReadFileSafe(string fileName)
+               {
+                   if (!File.Exists(fileName))
+                   {
+                       throw new FileNotFoundException();
+                   }
+                   int CurrentRow = 0;
+                   bool ColumnsLimitIsSet = false;
+                   int ColumnsLimit = 0;
 
-        public int Max
-        {
-            get
-            {
-                int max = a[0, 0];
-                for (int i = 0; i < a.GetLength(0); i++)
-                {
-                    for (int j = 0; j < a.GetLength(1); j++)
-                    {
-                        if (a[i, j] > max) max = a[i, j];
-                    }
-                }
-                return max;
-            }
-        }
+                   using (StreamReader streamReader = new StreamReader(fileName))
+                   {
+                       if (streamReader.Peek() == -1)
+                       {
+                           throw new ArgumentException($"Файл {fileName} пустой.");
+                       }
 
-        public void MaxElementNumber(ref List<int> indexNumber)
-        {
-            for (int i = 0; i < a.GetLength(0); i++)
-            {
-                for (int j = 0; j < a.GetLength(1); j++)
-                {
-                    if (a[i, j] == Max)
-                    {
-                        indexNumber.Add((i * a.GetLength(1)) + j + 1);
-                    }
-                }
-            }
-        }
+                       while (streamReader.Peek() >= 0)
+                       {
+                           string LineToParse = streamReader.ReadLine();
+                           string[] Elements = LineToParse.Split(',');
+
+                           if (!ColumnsLimitIsSet)
+                           {
+                               ColumnsLimit = Elements.Length;
+                               if (ColumnsLimit == 0)
+                               {
+                                   throw new ArgumentException($"Неверное количество колонок в {fileName} в ряду {CurrentRow + 1}, должна быть хотя бы 1 колонка.");
+                               }
+                           }
+
+                           if (Elements.Length != ColumnsLimit)
+                           {
+                               throw new ArgumentException($"Неверное количество колонок в {fileName} в ряду {CurrentRow + 1}, должно быть {ColumnsLimit} колонок");
+                           }
+                           int[] ParseIntegers;
+
+                           try
+                           {
+                               ParseIntegers = Array.ConvertAll(Elements, int.Parse);
+                           }
+                           catch
+                           {
+                               throw new ArgumentException($"Файл {fileName} содержет неверные данные в ряду {CurrentRow + 1}");
+                           }
+
+                           for (int i = 0; i < ParseIntegers.Length; i++)
+                           {
+                               yield return (row: CurrentRow, column: i, value: ParseIntegers[i]);
+                           }
+                           CurrentRow++;
+                       }
+                   }
+               }
+
+
+
+               private static (uint rowsTotal, uint columnsTotal) ParseDimentions(string fileName)
+               {
+                   if (!File.Exists(fileName))
+                   {
+                       throw new FileNotFoundException();
+                   }
+
+                   uint RowsTotal = 0;
+                   uint ColumnsTotal = 0;
+
+                   bool ColumnsLimitIsSet = false;
+
+                   using (StreamReader streamReader = new StreamReader(fileName))
+                   {
+                       if (streamReader.Peek() == -1)
+                       {
+                           throw new ArgumentException($"Файл {fileName} пустой.");
+                       }
+
+                       while (streamReader.Peek() >= 0)
+                       {
+                           RowsTotal++;
+
+                           String s = streamReader.ReadLine();
+                           if (!ColumnsLimitIsSet)
+                           {
+                               string[] elements = s.Split(',');
+
+                               ColumnsTotal = (uint)elements.Length;
+
+                               if (ColumnsTotal == 0)
+                               {
+                                   throw new ArgumentException($"Неверное число колонок в {fileName} в строке {RowsTotal} должна быть хотя бы 1 колонка.");
+                               }
+                           }
+                       }
+                   }
+
+                   return (rowsTotal: RowsTotal, columnsTotal: ColumnsTotal);
+
+               }
+
+               public string OutputdataToDirectory(string folderName)
+               {
+                   if (!Directory.Exists(folderName))
+                   {
+                       Directory.CreateDirectory(folderName);
+                   }
+
+                   string fileName = Path.GetRandomFileName();
+                   string pathString = Path.Combine(folderName, fileName);
+
+                   using (StreamWriter file = new StreamWriter(@pathString))
+                   {
+                       for (int i = 0; i < Data.GetLength(0); i++)
+                       {
+                           string line = "";
+
+                           for (int j = 0; j < Data.GetLength(1); j++)
+                           {
+                               line += Data[i, j] + ",";
+                           }
+
+                           line = line.TrimEnd(',');
+                           file.WriteLine(line);
+
+                       }
+                   }
+
+                   return pathString;
+               }
+
+               public int Summ()
+               {
+                   int result = 0;
+                   for (int i = 0; i < Data.GetLength(0); i++)
+                   {
+                       for (int j = 0; j < Data.GetLength(1); j++)
+                       {
+                           result += Data[i, j];
+                       }
+                   }
+                   return result;
+               }
+
+               public int SummMoreThan(int n)
+               {
+                   int result = 0;
+                   for (int i = 0; i < Data.GetLength(0); i++)
+                   {
+                       for (int j = 0; j < Data.GetLength(1); j++)
+                       {
+                           if (Data[i, j] > n)
+                           {
+                               result += Data[i, j];
+                           }
+                       }
+                   }
+                   return result;
+               }
+
+               public int Min
+               {
+                   get
+                   {
+                       int min = Data[0, 0];
+                       for (int i = 0; i < Data.GetLength(0); i++)
+                       {
+                           for (int j = 0; j < Data.GetLength(1); j++)
+                           {
+                               if (Data[i, j] < min) min = Data[i, j];
+                           }
+                       }
+                       return min;
+                   }
+
+               }
+
+               public int Max
+               {
+                   get
+                   {
+                       int max = Data[0, 0];
+                       for (int i = 0; i < Data.GetLength(0); i++)
+                       {
+                           for (int j = 0; j < Data.GetLength(1); j++)
+                           {
+                               if (Data[i, j] > max) max = Data[i, j];
+                           }
+                       }
+                       return max;
+                   }
+               }
+
+               public void MaxElementNumber(ref List<int> indexNumber)
+               {
+                   for (int i = 0; i < Data.GetLength(0); i++)
+                   {
+                       for (int j = 0; j < Data.GetLength(1); j++)
+                       {
+                           if (Data[i, j] == Max)
+                           {
+                               indexNumber.Add((i * Data.GetLength(1)) + j + 1);
+                           }
+                       }
+                   }
+               }*/
     }
 }
