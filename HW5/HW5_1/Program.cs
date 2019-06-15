a68ec3a2823f1695375c9d9ec6715a2b8da4a71f
Naragas <naragas@gmail.com>
2019-06-15 14:48:26 +0300

    в

---
+++ HW5/HW5_1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HW5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Логин должен состоять из букв латинского алфавита и цифр.\nПервый симв не должен быть цифрой.\nДлина пароля должна быть от 2 до 10 символов.\nВведите ваш логин: ");
+            string userLogin = Console.ReadLine();
+
+            // loginChaeck(userLogin);
+
+            regulLoginCheck(userLogin);
+        }
+
+        private static void regulLoginCheck(string userLogin)
+        {
+            Regex myLoginCheck = new Regex(@"^[a-zA-Z]{1}[a-zA-Z1-9]{1,9}");
+            if (myLoginCheck.IsMatch(userLogin))
+            {
+                Console.WriteLine("Формат ввода логина корректен.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Длина пароля должа быть от 2 до 10 символов и первый символ пароля не должен быть цифрой.");
+                Console.ReadKey();
+            }
+        }
+
+        private static void loginChaeck(string userLogin)
+        {
+            char[] login = userLogin.ToArray();
+
+            if (login.Length >= 2 && login.Length <= 10 && !char.IsDigit(login[0]))
+            {
+                for (int i = 0; i < login.Length; i++)
+                {
+                    if (login[i] >= 'a' && login[i] <= 'z' || login[i] >= 'A' && login[i] <= 'Z' || char.IsDigit(login[i])) ;
+
+                }
+                Console.WriteLine("Формат ввода логина корректен.");
+                Console.WriteLine("Пароль должен состоять только из цифр и латинских букв.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Длина пароля должа быть от 2 до 10 символов и первый символ пароля не должен быть цифрой.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
