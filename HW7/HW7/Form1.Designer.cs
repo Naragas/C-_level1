08ac14abc53d5359a5a7ecb4cd2e9776a1840409
Naragas <naragas@gmail.com>
2019-06-20 18:02:40 +0300

    123

---
+++ HW7/HW7/Form1.Designer.cs
@@ -0,0 +1,35 @@
+﻿namespace HW7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private System.Windows.Forms.Button btnCommand1;
+        private System.Windows.Forms.Button btnCommand2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label countlbl;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удвоительToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem игратьToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ HW7/HW7/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnCommand1.Text = "+1";
+            btnCommand2.Text = "x2";
+            btnReset.Text = "Сброс";
+            lblNumber.Text = "1";
+            countlbl.Text = "0";
+            this.Text = "Удвоитель";
+        }
+
+        private void btnCommand1_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+            countlbl.Text = (int.Parse(countlbl.Text) + 1).ToString();
+        }
+        private void btnCommand2_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
+            countlbl.Text = (int.Parse(countlbl.Text) + 1).ToString();
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = "1";
+            countlbl.Text = "0";
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnCommand1 = new System.Windows.Forms.Button();
+            this.btnCommand2 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.countlbl = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удвоительToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.игратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCommand1
+            // 
+            this.btnCommand1.Location = new System.Drawing.Point(197, 36);
+            this.btnCommand1.Name = "btnCommand1";
+            this.btnCommand1.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand1.TabIndex = 0;
+            this.btnCommand1.Text = "button1";
+            this.btnCommand1.UseVisualStyleBackColor = true;
+            this.btnCommand1.Click += new System.EventHandler(this.btnCommand1_Click);
+            // 
+            // btnCommand2
+            // 
+            this.btnCommand2.Location = new System.Drawing.Point(197, 65);
+            this.btnCommand2.Name = "btnCommand2";
+            this.btnCommand2.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand2.TabIndex = 1;
+            this.btnCommand2.Text = "button2";
+            this.btnCommand2.UseVisualStyleBackColor = true;
+            this.btnCommand2.Click += new System.EventHandler(this.btnCommand2_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(197, 94);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "button3";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(13, 36);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(35, 13);
+            this.lblNumber.TabIndex = 3;
+            this.lblNumber.Text = "label1";
+            // 
+            // countlbl
+            // 
+            this.countlbl.AccessibleDescription = "Ходы";
+            this.countlbl.AutoSize = true;
+            this.countlbl.Location = new System.Drawing.Point(74, 235);
+            this.countlbl.Name = "countlbl";
+            this.countlbl.Size = new System.Drawing.Size(34, 13);
+            this.countlbl.TabIndex = 4;
+            this.countlbl.Text = "Ходы";
+            this.countlbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.удвоительToolStripMenuItem,
+            this.игратьToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // удвоительToolStripMenuItem
+            // 
+            this.удвоительToolStripMenuItem.Name = "удвоительToolStripMenuItem";
+            this.удвоительToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.удвоительToolStripMenuItem.Text = "Удвоитель";
+            // 
+            // игратьToolStripMenuItem
+            // 
+            this.игратьToolStripMenuItem.Name = "игратьToolStripMenuItem";
+            this.игратьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.игратьToolStripMenuItem.Text = "Играть";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.countlbl);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCommand2);
+            this.Controls.Add(this.btnCommand1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
